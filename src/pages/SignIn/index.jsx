ddaf9139303e61079928bdceedda935e14140950
Wesam-abutuaimeh <wesam@ucst.edu.ps>
2023-07-07 11:43:55 +0300

    Customize project structure

---
+++ src/pages/SignIn/index.jsx
@@ -0,0 +1,10 @@
+import { Component } from "react";
+import "./style.css"
+
+class SignIN extends Component {
+    render() {
+        return <div></div>;
+    }
+}
+
+export default SignIN;
---
+++ src/pages/SignUp/index.jsx
@@ -0,0 +1,10 @@
+import { Component } from "react";
+import "./style.css"
+
+class SignUp extends Component {
+    render() {
+        return <div></div>;
+    }
+}
+
+export default SignUp;
