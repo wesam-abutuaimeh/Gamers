2a0211fe5d4d9e2ef8d702e5819afd0e77749a4f
Wesam-abutuaimeh <wesam@ucst.edu.ps>
2023-07-08 18:53:36 +0300

    Doing Picture component

---
+++ src/components/Picture/index.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class Picture extends Component {
+
+    render() {
+        return (<img src={this.props.src} alt={this.props.alt} />);
+    }
+}
+
+export default Picture;
