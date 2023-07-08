8ce1c10aac536078a322b976a009e90b9789d4ae
Wesam-abutuaimeh <wesam@ucst.edu.ps>
2023-07-08 18:52:51 +0300

    Doing Orline component

---
+++ src/components/OrLine/index.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+import './style.css'
+
+class OrLine extends Component {
+    render() {
+        return <div className='or__box'><span>Or</span></div>
+    }
+}
+export default OrLine;
