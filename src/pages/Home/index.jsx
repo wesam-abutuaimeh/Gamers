c34d6e04abc967edda70a2234101bce8115841d3
Wesam-abutuaimeh <wesam@ucst.edu.ps>
2023-07-08 18:54:56 +0300

    Add Home into App.js

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import SignIn from "../../Layout/SignIn";
+
+class Home extends Component {
+
+    state = {}
+    render() {
+        return (<>
+            <SignIn />
+        </>);
+    }
+}
+
+export default Home;
