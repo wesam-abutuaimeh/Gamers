53d12c06868758e32fb18e6cfb7d1e505d31c39a
Wesam-abutuaimeh <wesam@ucst.edu.ps>
2023-07-09 12:02:07 +0300

    Use props

--- src/components/Picture/index.jsx
+++ src/components/Picture/index.jsx
@@ -2,8 +2,8 @@ import React, { Component } from "react";
 
 class Picture extends Component {
     render() {
-        const { src, alt } = this.props;
-        return (<img src={src} alt={alt} />);
+        const { src, alt, pictureClassName } = this.props;
+        return (<img src={src} alt={alt} className={pictureClassName} />);
     }
 }
 
