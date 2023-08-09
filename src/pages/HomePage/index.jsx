6b417e706fc78800f51dd566d356281dbe2befda
Wesam-abutuaimeh <wesam@ucst.edu.ps>
2023-08-09 19:01:32 +0300

    HomePage

---
+++ src/pages/HomePage/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Nav from '../../components/Nav';
+import Header from '../../components/Header';
+// import { useThemeContext } from '../../contexts/ThemeContext'
+import LastPlayed from '../../components/LastPlayed';
+import RecentTrophy from '../../components/RecentTrophy';
+import PurpleFriendChat from '../../components/FriendPurple';
+import Slider from '../../components/Silder';
+import "./style.css"
+
+const HomePage = () => {
+    // const { themeMode, toggleMode } = useThemeContext();
+    return (
+        <div className='homepage'>
+            <Nav />
+            <main>
+                <Header />
+                <Slider color='#d8d8d8' />
+                <div className='statistics__container'>
+                    <LastPlayed color='#d8d8d8' />
+                    <RecentTrophy color='#d8d8d8' />
+                    <PurpleFriendChat color='#d8d8d8' />
+                </div>
+            </main>
+        </div >
+    )
+}
+
+export default HomePage
