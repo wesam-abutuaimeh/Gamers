6a1cc04b2c861dc3aff6b5466b7c2f2cee9db280
Wesam-abutuaimeh <wesam@ucst.edu.ps>
2023-08-09 19:05:35 +0300

    edit routes

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -1,14 +1,77 @@
-import { PATHS } from "./PATHS"
-import SignIn from "../pages/SignIn"
-import SignUp from "../pages/SignUp"
+import React from "react";
+import SignIn from "../pages/SignIn";
+import SignUp from "../pages/SignUp";
+import HomePage from "../pages/HomePage";
+import Profile from "../pages/Profile";
+import Settings from "../pages/Settings";
+import ErrorPage from "../pages/ErrorPage";
+import { PATHS } from "./PATHS";
+import UserGuard from "../components/Guards/UserGuard";
+import GuestGuard from "../components/Guards/GuestGuard";
+import AdminGuard from "../components/Guards/AdminGuard";
+
+const adminPages = [
+    // {
+    //     path: PATHS.ADMIN.ROOT,
+    //     element: Settings,
+    //     children: [
+    //         {
+    //             path: PATHS.ADMIN.USERS,
+    //             element: <h1>Users</h1>,
+    //         },
+    //     ],
+    // },
+    {
+        path: "/",
+        element: <AdminGuard />,
+        children: [{
+            path: PATHS.ADMIN.ROOT,
+            element: <Settings />,
+            index: true,
+        }]
+    }
+];
+
+const guestPages = [
+
+    {
+        path: "/",
+        element: <GuestGuard />,
+        children: [{
+
+            element: <SignUp />,
+            index: true,
+        },
+        {
+            path: PATHS.Auth.SIGNIN,
+            element: <SignIn />,
+        },]
+    }
+];
+
+const userPages = [
+    {
+        path: PATHS.Home,
+        element: <UserGuard />,
+        children: [
+            {
+                index: true,
+                element: <HomePage />,
+            },
+            {
+                path: PATHS.PROFILE,
+                element: <Profile />,
+            }
+        ],
+    },
+];
 
 export const routes = [
+    ...guestPages,
+    ...userPages,
+    ...adminPages,
     {
-        path: PATHS.Auth.SIGNIN,
-        element: <SignIn />,
+        path: PATHS.NOT_FOUND["*"],
+        element: <ErrorPage />,
     },
-    {
-        path: PATHS.Auth.SIGNUP,
-        element: <SignUp />,
-    }
-]
+];
