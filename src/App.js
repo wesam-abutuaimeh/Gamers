f47314b815df6d8d2403f23809de75d50df9bb19
Wesam-abutuaimeh <wesam@ucst.edu.ps>
2023-08-09 20:11:16 +0300

    Update theme context scoap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { Router } from "./router";
+import AuthContext from "./contexts/AuthContext";
+import { useThemeContext } from "./contexts/ThemeContext";
 
 function App() {
+  const { themeMode } = useThemeContext();
   return (
-    <div className="App">
-      <Router />
+    <div className={themeMode}>
+      <AuthContext>
+        <Router />
+      </AuthContext>
     </div>
   );
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { ThemeContextProvider } from "./contexts/ThemeContext";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeContextProvider>
+        <App />
+      </ThemeContextProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
