3a012061e5686d057d93f955c1be1002431d3733
Wesam-abutuaimeh <wesam@ucst.edu.ps>
2023-08-09 12:16:03 +0300

    signin page with vaildation

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -3,48 +3,71 @@ import { Link } from "react-router-dom";
 import Logo from "../../components/Logo";
 import Quote from "../../components/Quotes";
 import Picture from "../../components/Picture";
-import InputField from "../../components/InputField";
 import Button from "../../components/Button";
 import OrLine from "../../components/OrLine";
+import { useAuthContext } from "../../contexts/AuthContext";
+import { END_POINTS } from "../../constant/auth";
+import { signinSchema } from "../../constant/authFormsValidation";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 import "./style.css";
 
 function SignIn() {
+    const { handleAUTHENTICATE, isLoading } = useAuthContext();
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({
+        resolver: yupResolver(signinSchema),
+    });
+
+    const onSubmit = async (data) => {
+        handleAUTHENTICATE(END_POINTS.LOGIN, data);
+    };
+
     return (
         <div className="sign__in__container">
             <div className="left__side">
-                <Logo>
-                    <Picture src="/assets/sign__in__logo.svg" alt="sigin logo" />
-                </Logo>
-                <Quote color="#696F79" fontStyle="itilac" signatureColor="#696F79" />
-                <Picture src="/assets/sign_in_img.png" alt="sigin mage" pictureClassName="sign_in_img" />
+                <Logo><Picture src="/assets/sign__in__logo.svg" alt="signin logo" /></Logo>
+                <Quote color="#696F79" fontStyle="italic" signatureColor="#696F79" />
+                <Picture src="/assets/sign_in_img.png" alt="signin image" pictureClassName="sign_in_img" />
             </div>
             <div className="right__side">
                 <div className="registration__area">
                     <h1>Join the game!!</h1>
-                    <p> Go inside the best gamers social network! </p>
+                    <p>Go inside the best gamers social network!</p>
                     <div className="social__media__links__container">
-                        <Link className="social__media__link" to="https://google.com">
-                            <Picture src="/assets/flat-color-icons_google.png" alt="google" />
-                        </Link>
-                        <Link className="social__media__link" to="https://google.com">
-                            <Picture src="/assets/logos_twitter.png" alt="twitter" />
-                        </Link>
-                        <Link className="social__media__link" to="https://google.com">
-                            <Picture src="/assets/cib_linkedin-in.png" alt="linkedin" />
-                        </Link>
-                        <Link className="social__media__link" to="https://google.com">
+                        <a className="social__media__link" href="https://google.com" target="_blank" rel="noopener noreferrer" >
+                            <Picture src="/assets/flat-color-icons_google.png" alt="Google" />
+                        </a>
+                        <a className="social__media__link" href="https://twitter.com" target="_blank" rel="noopener noreferrer" >
+                            <Picture src="/assets/logos_twitter.png" alt="Twitter" />
+                        </a>
+                        <a className="social__media__link" href="https://linkedin.com" target="_blank" rel="noopener noreferrer" >
+                            <Picture src="/assets/cib_linkedin-in.png" alt="LinkedIn" />
+                        </a>
+                        <a className="social__media__link" href="https://github.com" target="_blank" rel="noopener noreferrer" >
                             <Picture src="/assets/ant-design_github-filled.png" alt="Github" />
-                        </Link>
+                        </a>
                     </div>
                     <OrLine />
-                    <form action="#">
-                        <InputField htmlFor="email" title="Your email" type="email"
-                            placeholder="Write your email" required={true} id="email" className="email" />
-                        <InputField htmlFor="create__password" title="Enter your password*" type="password"
-                            placeholder="•••••••••" required={true} id="create__password" className="create__password" />
-                        <Button type="submit" componentClassName="login__btn" bgcolor="#1565D8" color="#fff">Login</Button>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <label htmlFor="email">Your email</label>
+                        <input type="email" placeholder="Write your email" id="email" className="email" {...register("email")} />
+                        {errors.email && <span className="error__msg">{errors.email.message}</span>}
+
+                        <label htmlFor="password">Enter your password*</label>
+                        <input type="password" placeholder="•••••••••" id="create__password" className="create__password" {...register("password")} />
+                        {errors.password && <span className="error__msg">{errors.password.message} </span>}
+
+                        <Button type="submit" componentClassName="login__btn" bgcolor="#1565D8" color="#fff" >
+                            {isLoading ? "isLoading..." : "Login"}
+                        </Button>
                     </form>
-                    <div className="exist__account">Don’t have an account? <Link to="/signup">Register</Link></div>
+                    <div className="exist__account">
+                        Don’t have an account? <Link to="/">Register</Link>
+                    </div>
                 </div>
             </div>
         </div>
