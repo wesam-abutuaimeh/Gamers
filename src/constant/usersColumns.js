b94c71a5830fdd3cccbc2be41c761092ce07166b
Wesam-abutuaimeh <wesam@ucst.edu.ps>
2023-08-09 12:19:45 +0300

    Add  api paths , roles , themes , userColumn js

---
+++ src/constant/usersColumns.js
@@ -0,0 +1,16 @@
+const USERS_COLUMNS = (data) => [
+  {
+    key: "name",
+    title: "Name",
+  },
+  {
+    key: "email",
+    title: "Email",
+  },
+  {
+    key: "isAdmin",
+    title: "Role",
+  },
+];
+
+export default USERS_COLUMNS;
