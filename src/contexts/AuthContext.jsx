7a22be8ad70361bde6d1b6845074e8bc14b01a89
Wesam-abutuaimeh <wesam@ucst.edu.ps>
2023-08-09 19:06:07 +0300

    auth context

---
+++ src/contexts/AuthContext.jsx
@@ -0,0 +1,14 @@
+import { createContext, useContext } from "react";
+import useAuth from "../hooks/useAuth"
+
+export const RoleContext = createContext(null);
+export const useAuthContext = () => useContext(RoleContext);
+
+const AuthContext = ({ children }) => {
+    const data = useAuth();
+    return <RoleContext.Provider value={data}>
+        {children}
+    </RoleContext.Provider>
+}
+
+export default AuthContext;
