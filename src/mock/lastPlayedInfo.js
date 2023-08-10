5f5cc1c4fb7cb121d6ea87ab4bcd1c72bd31a360
Wesam-abutuaimeh <wesam@ucst.edu.ps>
2023-08-10 19:49:15 +0300

    add id for obj

--- src/mock/lastPlayedInfo.js
+++ src/mock/lastPlayedInfo.js
@@ -1,17 +1,21 @@
 export const LAST_PLAYED = [
   {
+    id: 1,
     content: "Hogwarts Legacy 50%",
     avatar: "/assets/Ellipse_4.png",
   },
   {
+    id: 2,
     content: "God Of War: Ragnarök 72.5%",
     avatar: "/assets/god_of_war_mini.png",
   },
   {
+    id: 3,
     content: "Crash Bandicoot N. Sane Trilogy 34%",
     avatar: "/assets/Ellipse_4.png",
   },
   {
+    id: 4,
     content: "Dying Light 2 Stay Human 100%",
     avatar: "/assets/DYING_LIGHT_MINI.png",
   },
