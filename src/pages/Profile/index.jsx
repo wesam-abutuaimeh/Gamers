cb13271392af344c525ad820b857cb17bd5309bd
Wesam-abutuaimeh <wesam@ucst.edu.ps>
2023-08-11 21:47:24 +0300

    Init profile page for  future work

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { ROLES } from '../../constant/roles';
+import { useNavigate } from 'react-router-dom';
 import "./style.css"
 
 const Profile = () => {
-    return (
-        <div>Profile</div>
-    )
-}
+    const navigate = useNavigate();
+    useEffect(() => {
+        const role = localStorage.getItem('role');
+        if (!role || role === ROLES.GUEST) {
+            navigate('/login');
+            setTimeout(() => {
+                alert('Hey Guest, sign in before!');
+            }, 1000);
+        }
+    });
 
-export default Profile
+    const handleGoHome = () => {
+        navigate('/');
+    };
+
+    return (
+        <div className='profile'>
+            <p className='note'>
+                Under Development ...
+                here well be  games related with this user and featuers depend on api updates Godwilling
+            </p>
+            <div className='back'>
+                <button onClick={handleGoHome}>Back to Home</button>
+            </div>
+        </div>
+    );
+};
+export default Profile;
