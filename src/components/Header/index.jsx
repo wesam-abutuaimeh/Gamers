03f3f6f275574534f48d94e7b36605f173e18d2e
Wesam-abutuaimeh <wesam@ucst.edu.ps>
2023-08-11 21:46:58 +0300

    Doing Header to navgite to profile page

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import { PATHS } from '../../router/PATHS'
+import { Link } from 'react-router-dom'
 import "./style.css"
 
 const Header = () => {
     return (
         <header className='header'>
-            <div className="user__info">
-                <p>Welcome back,</p>
-                <span>Jenny!</span>
-            </div>
+            <Link to={PATHS.PROFILE}>
+                <div className="user__info">
+                    <p>Welcome back,</p>
+                    <span>Jenny!</span>
+                </div>
+            </Link>
             <img src="/assets/avatar02.png" alt="avatar" className="avatar" />
         </header>
     )
