658cbea571eebf56f523d597b89b4ae60b6ceb8e
Wesam-abutuaimeh <wesam@ucst.edu.ps>
2023-08-13 21:06:22 +0300

    Show user name in home page

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,31 @@
-import React from 'react'
-import { PATHS } from '../../router/PATHS'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import { PATHS } from '../../router/PATHS'
+import { API_URL } from "../../config/api"
+import { END_POINTS } from '../../constant/auth'
+import useAPI from "../../hooks/useAPI"
 import "./style.css"
 
+
 const Header = () => {
+    const { get, data } = useAPI(API_URL + END_POINTS.PROFILE);
+    const config = {
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+        }
+    }
+
+    useEffect(() => {
+        get(config);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [config.headers.Authorization])
+
     return (
-        <header className='header'>
+        <header className="header">
             <Link to={PATHS.PROFILE}>
                 <div className="user__info">
                     <p>Welcome back,</p>
-                    <span>Jenny!</span>
+                    {data.name && <span>{data.name} !</span>}
                 </div>
             </Link>
             <img src="/assets/avatar02.png" alt="avatar" className="avatar" />
