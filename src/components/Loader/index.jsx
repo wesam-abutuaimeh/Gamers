1795b0601d2857d8a5f421071548ac0a967fa66d
Wesam-abutuaimeh <wesam@ucst.edu.ps>
2023-08-13 21:03:41 +0300

    Add loader divs

---
+++ src/components/Loader/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./style.css"
+
+const Loader = () => {
+    return (
+        <div className="preloader">
+            <div className="waterfall">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    )
+}
+
+export default Loader
