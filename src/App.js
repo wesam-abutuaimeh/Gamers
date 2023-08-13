20035bd08b99b91963ec25ced110a8287996baf1
Wesam-abutuaimeh <wesam@ucst.edu.ps>
2023-08-13 21:06:01 +0300

    Add Suspense

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Router } from "./router";
 import { useThemeContext } from "./contexts/ThemeContext";
+import Loader from "./components/Loader";
 
 function App() {
   const { themeMode } = useThemeContext();
   return (
     <div className={`App ${themeMode}`}>
-      <Router />
+      <Suspense fallback={<Loader />}>
+        <Router />
+      </Suspense>
     </div>
   );
 }
