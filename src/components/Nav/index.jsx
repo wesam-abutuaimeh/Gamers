925c4d4b91ce0aaf92df9b31d2da61859772529c
Wesam-abutuaimeh <wesam@ucst.edu.ps>
2023-08-15 21:56:27 +0300

    use logout direct alter use function

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -7,9 +7,6 @@ import "./style.css"
 const Nav = () => {
   const { toggleMode } = useThemeContext();
   const { logout } = useAuth();
-  const handleLogout = () => {
-    logout()
-  }
   return (
     <aside>
       <nav>
@@ -20,7 +17,7 @@ const Nav = () => {
           <Link to=""><img src="/assets/Like.png" alt="favorite" className="favorite" /></Link>
           <Link to=""><img src="/assets/Setting.png" alt="Setting" className="Setting" /></Link>
           <Link to=""><img src="/assets/Puzzle.png" alt="Puzzle" className="Puzzle" /></Link>
-          <span className="logout__btn" onClick={handleLogout}>
+          <span className="logout__btn" onClick={logout}>
             <svg className="MuiSvgIcon-root MuiSvgIcon-fontSizeSmall MuiBox-root css-uqopch" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="LogoutIcon"><path d="m17 7-1.41 1.41L18.17 11H8v2h10.17l-2.58 2.58L17 17l5-5zM4 5h8V3H4c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h8v-2H4V5z"></path></svg>
           </span>
         </div>
