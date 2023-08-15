282d973b64bf379ae8d44f58d5a465ba7ae0bfe3
Wesam-abutuaimeh <wesam@ucst.edu.ps>
2023-08-15 13:17:41 +0300

    Add inputs as constant for signup & in

---
+++ src/constants/loginInputs.js
@@ -0,0 +1,16 @@
+export const INPUTS = [
+  {
+    id: "email",
+    class: "email",
+    label: "Your email",
+    type: "email",
+    placeholder: "Write your email",
+  },
+  {
+    id: "create__password",
+    class: "create__password",
+    label: "Enter your password*",
+    type: "password",
+    placeholder: "•••••••••",
+  },
+];
---
+++ src/constants/signupInputs.js
@@ -0,0 +1,37 @@
+export const INPUTS = [
+  {
+    id: "username",
+    class: "username",
+    label: "Username*",
+    type: "text",
+    placeholder: "Enter username",
+  },
+  {
+    id: "email",
+    class: "email",
+    label: "Email Address*",
+    type: "email",
+    placeholder: "Enter email address",
+  },
+  {
+    id: "phone__number",
+    class: "phone__number",
+    label: "Phone Number*",
+    type: "text",
+    placeholder: "970 598-844-683",
+  },
+  {
+    id: "create__password",
+    class: "create__password",
+    label: "Create Password*",
+    type: "password",
+    placeholder: "password",
+  },
+  {
+    id: "repeat__password",
+    class: "create__password",
+    label: "Repeat password",
+    type: "password",
+    placeholder: "Repeat password",
+  },
+];
