ffc7106213bb4d9cab42b4f60fc0b5863a3b1d66
Wesam-abutuaimeh <wesam@ucst.edu.ps>
2023-08-15 21:55:34 +0300

    change class  from componentClassName  to class

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./style.css";
 
-const Button = ({ type, componentClassName, bgcolor, color, shadow, onClick, children, }) => {
-    return <button type={type} className={componentClassName} style={{ backgroundColor: bgcolor, color: color, boxShadow: shadow }} onClick={onClick}>
+const Button = ({ type, className, bgcolor, color, shadow, onClick, children, }) => {
+    return <button type={type} className={className} style={{ backgroundColor: bgcolor, color: color, boxShadow: shadow }} onClick={onClick}>
         {children}
     </button>
 }
