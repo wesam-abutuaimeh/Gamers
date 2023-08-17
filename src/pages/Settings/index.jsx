ed0ab131ac077044a52d5a6507bdef8fd3ee60f7
Wesam-abutuaimeh <wesam@ucst.edu.ps>
2023-08-17 13:23:08 +0300

    Doing Pagination

--- src/pages/Settings/index.jsx
+++ src/pages/Settings/index.jsx
@@ -1,51 +1,55 @@
 import React, { useEffect, Suspense, lazy, useState } from 'react';
 import useAPI from '../../hooks/useAPI';
-import { API_URL } from "../../config/api"
+import { API_URL } from "../../config/api";
 import USERS_COLUMNS from "../../constants/usersColumns";
 import Nav from "../../components/Nav";
 import "./style.css";
 import CustomAlert from '../../components/CustomAlert';
 const Table = lazy(() => import("../../components/Table"));
 
-export default function Setting() {
-    const { data, get, getQuery, isLoading, deleteItem } = useAPI(API_URL);
+function Setting() {
     const [value, setValue] = useState("");
     const [showAlert, setShowAlert] = useState(false);
     const [idToDelete, setIdToDelete] = useState(null);
-
+    const [currentPage, setCurrentPage] = useState(1);
+    const { data, get, getQuery, isLoading, deleteItem } = useAPI(API_URL);
     const adminToken = localStorage.getItem("token");
-
+    const PER_PAGE = 10;
     const config = {
         headers: {
             Authorization: `Bearer ${adminToken}`
         }
-    }
+    };
 
     const loadData = () => {
-        get(config);
-    }
+        get({ ...config, params: { page: currentPage, limit: PER_PAGE } });
+    };
 
     const handleDelete = (id) => {
         setShowAlert(true);
-        setIdToDelete(id)
+        setIdToDelete(id);
     };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        getQuery(value, config)
-    }
+        getQuery(value, config);
+    };
 
     const handleReset = () => {
         loadData();
-    }
+    };
+
+    const handlePageChange = (newPage) => {
+        setCurrentPage(newPage);
+    };
 
     useEffect(() => {
         loadData();
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+    }, [currentPage, PER_PAGE]);
 
     return (
-        <div className='settings__container' >
+        <div className='settings__container'>
             <Nav />
             {isLoading ? (
                 <h1 className="loader">Loading...</h1>
@@ -57,13 +61,24 @@ export default function Setting() {
                         <p>Total Pages: {data?.pages}</p>
                     </div>
                     <form className='search' onSubmit={handleSubmit}>
-                        <input type='text' onChange={(e) => setValue(e.target.value)} />
+                        <input type='text' value={value} onChange={(e) => setValue(e.target.value)} />
                         <button type='submit' className='search__btn'>Search</button>
                         <button type='reset' className='reset__btn' onClick={handleReset}>Reset</button>
                     </form>
                     <Suspense fallback={"Loading Table Data ..."}>
                         <Table columns={USERS_COLUMNS(data, handleDelete)} data={data.users || []} />
                     </Suspense>
+                    <div className="pagination">
+                        <button className="pagination-button" onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1}>
+                            Previous
+                        </button>
+                        <span className="pagination-info">
+                            Page {currentPage} of {data?.pages || 1}
+                        </span>
+                        <button className="pagination-button" onClick={() => handlePageChange(currentPage + 1)} disabled={currentPage === (data?.pages || 1)}>
+                            Next
+                        </button>
+                    </div>
                     {data.error && <span className="error__message">{data.error}</span>}
                     {showAlert && (
                         <CustomAlert
@@ -86,3 +101,4 @@ export default function Setting() {
         </div>
     );
 }
+export default Setting;
