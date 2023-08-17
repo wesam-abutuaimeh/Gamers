34d405ab72e75a01ef7992959cf0d78fcae6d9f1
Wesam-abutuaimeh <wesam@ucst.edu.ps>
2023-08-17 13:43:45 +0300

    Checking Password Strength

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -18,7 +18,7 @@ function SignUp(props) {
     const { handleAUTHENTICATE, isLoading } = useAuthContext();
     const {
         register,
-        handleSubmit,
+        handleSubmit, watch,
         formState: { errors },
     } = useForm({
         resolver: yupResolver(registrationSchema),
@@ -29,39 +29,24 @@ function SignUp(props) {
         console.log('din');
     };
 
+    const passwordValue = watch("password", "");
+    const hasUppercase = /[A-Z]/.test(passwordValue);
+    const hasLowercase = /[a-z]/.test(passwordValue);
+    const hasDigits = /\d/.test(passwordValue);
+    const hasSpecialChars = /[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]+/.test(passwordValue);
+    const isPasswordStrong =
+        hasUppercase && hasLowercase && hasDigits && hasSpecialChars && passwordValue.length >= 8;
+
     return (
         <div className="sign__up__container">
             <div className="left__side">
-                <Picture
-                    src="/assets/dots.png"
-                    alt="dots shape"
-                    pictureClassName="dots__shape"
-                />
-                <Picture
-                    src="/assets/circle__shape.png"
-                    alt="circle shape"
-                    pictureClassName="circle__shape"
-                />
+                <Picture src="/assets/dots.png" alt="dots shape" pictureClassName="dots__shape" />
+                <Picture src="/assets/circle__shape.png" alt="circle shape" pictureClassName="circle__shape" />
 
                 <Logo>
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="53"
-                        height="57"
-                        viewBox="0 0 53 57"
-                        fill="none">
-                        <path
-                            fillRule="evenodd"
-                            clipRule="evenodd"
-                            d="M36.3693 19.4904C33.9261 24.5722 28.7297 28.08 22.7141 28.08C14.351 28.08 7.57132 21.3004 7.57132 12.9373C7.57132 7.45807 10.4814 2.65854 14.8404 0C6.3926 1.58395 0 8.99844 0 17.906C0 27.9679 8.15675 36.1246 18.2186 36.1246C27.7467 36.1246 35.5664 28.8104 36.3693 19.4904Z"
-                            fill="white"
-                        />
-                        <path
-                            fillRule="evenodd"
-                            clipRule="evenodd"
-                            d="M43.7707 42.0109C39.3658 45.5307 33.1624 46.4385 27.7417 43.8302C20.2056 40.2041 17.036 31.1553 20.6621 23.6192C23.0378 18.6818 27.7412 15.6187 32.8218 15.113C24.5226 12.8775 15.5473 16.787 11.685 24.8137C7.32234 33.8806 11.1358 44.7674 20.2027 49.1301C28.7884 53.2613 39.0061 50.061 43.7707 42.0109Z"
-                            fill="white"
-                        />
+                    <svg xmlns="http://www.w3.org/2000/svg" width="53" height="57" viewBox="0 0 53 57" fill="none">
+                        <path fillRule="evenodd" clipRule="evenodd" d="M36.3693 19.4904C33.9261 24.5722 28.7297 28.08 22.7141 28.08C14.351 28.08 7.57132 21.3004 7.57132 12.9373C7.57132 7.45807 10.4814 2.65854 14.8404 0C6.3926 1.58395 0 8.99844 0 17.906C0 27.9679 8.15675 36.1246 18.2186 36.1246C27.7467 36.1246 35.5664 28.8104 36.3693 19.4904Z" fill="white" />
+                        <path fillRule="evenodd" clipRule="evenodd" d="M43.7707 42.0109C39.3658 45.5307 33.1624 46.4385 27.7417 43.8302C20.2056 40.2041 17.036 31.1553 20.6621 23.6192C23.0378 18.6818 27.7412 15.6187 32.8218 15.113C24.5226 12.8775 15.5473 16.787 11.685 24.8137C7.32234 33.8806 11.1358 44.7674 20.2027 49.1301C28.7884 53.2613 39.0061 50.061 43.7707 42.0109Z" fill="white" />
                     </svg>
                 </Logo>
                 <Quote color="#FFF" fontStyle="normal" />
@@ -93,6 +78,16 @@ function SignUp(props) {
                                 {errors[input["type"]] && <span className="error__msg">{errors[input["type"]].message}</span>}
                             </div>
                         })}
+
+                        <div>
+                            <div className="process">
+                                <div className="process__bar"></div>
+                            </div>
+                            <div className="text">
+                                {isPasswordStrong ? "Password is : Strong" : "You Must Write a Strong Password For Security, Please!"}
+                            </div>
+                        </div>
+
                         <div>
                             <input type="checkbox" id="agreement" className="agreement" required {...register("agreement")} />
                             <label htmlFor="agreement" className="agreement__label">
