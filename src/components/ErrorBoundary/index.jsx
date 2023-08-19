1c0bf0d5ee3995f6e62e30c0b9f5094b222b79da
Wesam-abutuaimeh <wesam@ucst.edu.ps>
2023-08-19 12:30:31 +0300

    Fix deployment issue

--- src/components/ErrorBoundary/index.jsx
+++ src/components/ErrorBoundary/index.jsx
@@ -1,4 +1,4 @@
-import React, { logErrorToMyService } from "react";
+import React from 'react';
 
 class ErrorBoundary extends React.Component {
     constructor(props) {
@@ -11,15 +11,28 @@ class ErrorBoundary extends React.Component {
     }
 
     componentDidCatch(error, errorInfo) {
-        logErrorToMyService(error, errorInfo);
+        this.setState({ error, errorInfo });
     }
 
     render() {
         if (this.state.hasError) {
-            return <h1>Whoops , Something went wrong!</h1>;
+            return (
+                <div className="error-boundary">
+                    <h1>Whoops, something went wrong!</h1>
+                    <p>We're sorry for the inconvenience. Please try reloading the page or contact our support team.</p>
+                    <button onClick={() => window.location.reload()}>Reload</button>
+                    <a href="mailto:support@example.com">Contact Support</a>
+                    <details>
+                        <summary>View Error Details</summary>
+                        <pre>{this.state.error && this.state.error.toString()}</pre>
+                        <pre>{this.state.errorInfo && this.state.errorInfo.componentStack}</pre>
+                    </details>
+                </div>
+            );
         }
 
         return this.props.children;
     }
 }
+
 export default ErrorBoundary;
